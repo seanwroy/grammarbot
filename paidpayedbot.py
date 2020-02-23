7cf71cc9c253f48cd18b1a2a24f3c86ae76bcd60
Sean Roy <seanwilliamroy@gmail.com>
2020-02-23 16:35:51 -0500

    Improved error handling, increased stability, improved how bot handles replies, added more comments

--- paidpayedbot.py
+++ paidpayedbot.py
@@ -5,7 +5,7 @@ import time
 import datetime
 import secrets
 from praw.exceptions import APIException
-from praw.models import Comment
+from praw.models import Comment, Message, ModmailMessage
 from multiprocessing import Process
 
 # Flag True to debug in testing subreddit
@@ -46,47 +46,32 @@ else:
         posts_replied = posts_replied.split("\n")
         posts_replied = list(filter(None, posts_replied))
 
-# Scrapes Inbox 
-def scrape_inbox(reddit):
-    for reply in reddit.inbox.unread(limit=None):
-        if isinstance(reply, Comment):
-            if reply.id not in posts_replied:
-                print(reply.author.name, "has replied with:", reply.body)
-                posts_replied.append(reply.id)
-                with open("replied_to.txt", "a") as f:
-                    f.write(reply.id + "\n")
-                if reply.body.lower() == 'delete':
-                    parent = reply.parent()  # the comment made by your bot
-                    parent.delete()
-                    print("Comment deleted upon request by", reply.author.name)
-                if reply.body.lower() == 'good bot':
-                    reply.reply(GOOD_BOT)
-                    print("Bot has replied with", GOOD_BOT)         
-                if reply.body.lower() == 'bad bot':
-                    reply.reply(BAD_BOT)
-                    print("Bot has replied with", BAD_BOT)
-            else:
-                break
-
-# Terms
+# Term array
 my_keywords = [' payed']
 
+# Received comments and messages arrays
+unread_comments = []
+unread_messages = []
+unread_modmessages = []
+
 # Scrapes comments from all subreddits
-def scrape_comments(reddit):
+def run_grammarbot(reddit):
+    print("Checking...")
     for comment in reddit.subreddit(SUB).stream.comments():
         if any(keyword in comment.body for keyword in my_keywords):
             if comment.id not in posts_replied:
                 if comment.author.name != reddit.user.me:
                     try:
                         comment.reply(MESSAGE_TEMPLATE)
-                        print("Replied to", comment.id, "written by", comment.author.name)
+                        print("Replied to", comment.id, "written by", comment.author.name, "in", SUB)
                     except APIException:
                         with open("auto_smbc_bot.log","a") as f:
                             f.write('{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()) + ": Rate Limit exception.\n")
-                            time.sleep(600) # Equals ten minutes, roughly equal to Rate Limit 
+                            time.sleep(600) # Equals ten minutes, roughly equal to Rate Limit
+                            run_grammarbot(reddit)
                     except Exception as e:
                         print("General exception caught: ", e)
-                        time.sleep(60)
+                        run_grammarbot(reddit)
                     if DEBUG:
                         print("Replied to: ", comment.id)
                     posts_replied.append(comment.id)
@@ -95,22 +80,45 @@ def scrape_comments(reddit):
                 if DEBUG:
                     print("Comment found: ", comment.id)
                 time.sleep(30)
-        else:
-            time.sleep(5)
-            break
+
+        # This loop handles all types of replies        
+        for reply in reddit.inbox.unread(limit=None):
+            print("Reply received...")
+
+            # Handles three types of replies: messages, modmail and comments -- only responds to comments
+            if isinstance(reply, Message):
+                print(reply.author.name, "has replied with a private message")
+                unread_messages.append(reply)
+                reddit.inbox.mark_read(unread_messages)
+                run_grammarbot(reddit)
+            if isinstance(reply, ModmailMessage):
+                print(reply.author.name, "has replied with a moderator message")
+                unread_modmessages.append(reply)
+                reddit.inbox.mark_read(unread_modmessages)
+                run_grammarbot(reddit)
+            if isinstance(reply, Comment):
+                if reply.id not in posts_replied:
+                    print(reply.author.name, "has replied with:", reply.body)
+                    posts_replied.append(reply.id)
+                    unread_comments.append(reply)
+                    reddit.inbox.mark_read(unread_comments)
+                    with open("replied_to.txt", "a") as f:
+                        f.write(reply.id + "\n")
+                    if reply.body.lower() == 'delete':
+                        parent = reply.parent()  # the comment made by your bot
+                        parent.delete()
+                        print("Comment deleted upon request by", reply.author.name)
+                    if reply.body.lower() == 'good bot':
+                        reply.reply(GOOD_BOT)
+                        print("Bot has replied with", GOOD_BOT)         
+                    if reply.body.lower() == 'bad bot':
+                        reply.reply(BAD_BOT)
+                        print("Bot has replied with", BAD_BOT)
+                    else:
+                        run_grammarbot(reddit)
+                else:
+                    break
         
-
-# Function which searches through comment across all subreddits looking for specific grammar mistake
-def run_grammarbot(reddit):
-    while True:
-        p = Process(target=scrape_comments(reddit))
-        p.start()
-
-        p2 = Process(target=scrape_inbox(reddit))
-        p2.start()
-
-        p.join()
-        p2.join()
 
 # Run while authenticated
 def main():
