315934c025d5b4e2d3beeb3b869d61cc6bd16e6e
Sean Roy <seanwilliamroy@gmail.com>
2020-02-23 00:34:41 -0500

    Passing array instead of string to add future terms in future and added additional comments

--- paidpayedbot.py
+++ paidpayedbot.py
@@ -18,7 +18,7 @@ else:
 MESSAGE_TEMPLATE = """/u/Paidpayedbot found a common grammar mistake in your comment. \n 
 ***** \n 
 **Paid** or **payed** is the past tense of 'to pay' depending on the implied meaning of 'pay'. 
-The first sense is the usual one of giving someone money while the second sense is to seal 
+The first form is the common meaning of giving someone money while the second form is to seal 
 (the deck or seams of a wooden ship) with pitch or tar to prevent leakage.\n
 You should almost always use **paid**, not **payed**.
 ***** \n
@@ -39,29 +39,34 @@ else:
         posts_replied = posts_replied.split("\n")
         posts_replied = list(filter(None, posts_replied))
 
-# Function which searches up to 100,000 comments across several subreddits looking for specific mistake
+# Terms
+my_keywords = [' payed']
+
+# Function which searches through comment across all subreddits looking for specific grammar mistake
 def run_grammarbot(reddit):
     while(True):
-        for comment in reddit.subreddit(SUB).comments(limit = 100000):
-            if "payed" in comment.body:
+        for comment in reddit.subreddit(SUB).stream.comments():
+            if any(keyword in comment.body for keyword in my_keywords):
                 if comment.id not in posts_replied:
-                    try:
-                        comment.reply(MESSAGE_TEMPLATE)
-                        print("Replied: ", comment.id)
-                    except APIException:
-                        with open("auto_smbc_bot.log","a") as f:
-                            f.write('{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()) + ": Rate Limit exception.\n")
-                    except Exception:
-                        print("General exception caught.")
+                    if comment.author.name != "paidpayedbot":
+                        try:
+                            comment.reply(MESSAGE_TEMPLATE)
+                            print("Replied to ", comment.id, " written by ", comment.author.name)
+                        except APIException:
+                            with open("auto_smbc_bot.log","a") as f:
+                                f.write('{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()) + ": Rate Limit exception.\n")
+                                time.sleep(600) # Equals ten minutes, roughly equal to Rate Limit 
+                        except Exception as e:
+                            print("General exception caught: ", e)
+                            time.sleep(60)
+                        if DEBUG:
+                            print("Replied to: ", comment.id)
+                        posts_replied.append(comment.id)
+                        with open("replied_to.txt", "a") as f:
+                            f.write(comment.id + "\n")
                     if DEBUG:
-                        print("Replied to: ", comment.id)
-                    posts_replied.append(comment.id)
-                    with open("replied_to.txt", "a") as f:
-                        f.write(comment.id + "\n")
-                if DEBUG:
-                    print("Comment found: ", comment.id)
-        time.sleep(900)
-
+                        print("Comment found: ", comment.id)
+                    time.sleep(30)
 # Run while authenticated
 def main():
     reddit = authenticate()
